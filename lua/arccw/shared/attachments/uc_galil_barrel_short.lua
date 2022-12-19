6145aa160f375c240a9a10a9487fcd14f7520fa3
Fesiug <publicfesiug@outlook.com>
2022-12-18 22:53:29 -0500

    galil changes

--- lua/arccw/shared/attachments/uc_galil_barrel_short.lua
+++ lua/arccw/shared/attachments/uc_galil_barrel_short.lua
@@ -16,7 +16,7 @@ att.Mult_SightTime						= 0.9
 att.Mult_SightedSpeedMult				= 1.1
 att.Mult_HipDispersion					= 0.85
 
-att.Mult_Recoil							= 1.25
+att.Mult_Recoil							= 1.15
 att.Mult_AccuracyMOA					= 1.5
 att.Mult_Range							= 0.75
 att.Mult_RPM							= 1.1
